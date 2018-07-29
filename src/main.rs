b6ca27958cbf4e23c233a687b175a5021f73f1a2
myuon <ioi.joi.koi.loi@gmail.com>
2018-07-29 22:02:26 +0900

    feat(all): sphere, camera and scene

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+extern crate rand;
+
 use std::fs;
 use std::io::{BufWriter, Write};
 
 mod vector;
 use vector::*;
 
-struct Ray {
-    point: V3,
+pub struct Ray {
+    origin: V3,
     direction: V3,
+}
+
+impl Ray {
+    pub fn extend_at(&self, scaler: f32) -> V3 {
+        self.origin + self.direction.scale(scaler)
+    }
 }
 
 struct Color(u8,u8,u8);
@@ -63,27 +71,121 @@ impl Renderer {
     }
 }
 
+pub struct HitRecord {
+    at: f32,
+    point: V3,
+    normal: V3,
+}
+
+pub trait Object {
+    fn hit(&self, ray: &Ray, tmin: f32, tmax: f32) -> Option<HitRecord>;
+}
+
+struct Sphere {
+    center: V3,
+    radius: f32,
+}
+
+impl Object for Sphere {
+    fn hit(&self, ray: &Ray, tmin: f32, tmax: f32) -> Option<HitRecord> {
+        let oc = ray.origin - self.center;
+        let a = ray.direction.square_norm();
+        let b = oc.dot(ray.direction);
+        let c = oc.square_norm() - self.radius * self.radius;
+        let discriminant = b*b - a*c;
+
+        if discriminant > 0.0 {
+            let check = |at| {
+                if tmin < at && at < tmax {
+                    let point = ray.extend_at(at);
+
+                    Some(HitRecord {
+                        at: at,
+                        point: point,
+                        normal: (point - self.center).scale(1.0 / self.radius)
+                    })
+                } else {
+                    None
+                }
+            };
+
+            check((-b - (b*b-a*c).sqrt()) / a).or(check((-b + (b*b-a*c).sqrt()) / a))
+        } else {
+            None
+        }
+    }
+}
+
+struct Scene {
+    objects: Vec<Box<dyn Object>>
+}
+
+impl Scene {
+    pub fn color(&self, ray: Ray) -> V3 {
+        for object in &self.objects {
+            if let Some(rec) = object.hit(&ray, 0.0, std::f32::MAX) {
+                return (rec.normal + V3(1.0, 1.0, 1.0)).scale(0.5);
+            }
+        }
+
+        let t = 0.5 * (ray.direction.normalize().y() + 1.0);
+        V3(1.0, 1.0, 1.0).scale(1.0 - t) + V3(0.5, 0.7, 1.0).scale(t)
+    }
+}
+
+struct Camera {
+    origin: V3,
+    lower_left_corner: V3,
+    horizontal: V3,
+    vertical: V3,
+}
+
+impl Camera {
+    pub fn new() -> Camera {
+        Camera {
+            origin: V3(0.0, 0.0, 0.0),
+            lower_left_corner: V3(-2.0, -1.0, -1.0),
+            horizontal: V3(4.0, 0.0, 0.0),
+            vertical: V3(0.0, 2.0, 0.0),
+        }
+    }
+
+    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+        Ray {
+            origin: self.origin,
+            direction: self.lower_left_corner + self.horizontal.scale(u) + self.vertical.scale(v)
+        }
+    }
+}
+
 fn main() {
     let w = 200;
     let h = 100;
+    let ns = 100;
 
-    let lower_left_corner = V3(-2.0, -1.0, -1.0);
-    let horizontal = V3(4.0, 0.0, 0.0);
-    let vertical = V3(0.0, 2.0, 0.0);
-    let origin = V3(0.0, 0.0, 0.0);
+    let camera = Camera::new();
+    let scene = Scene {
+        objects: vec![
+            Box::new(Sphere {
+                center: V3(0.0, 0.0, -1.0),
+                radius: 0.5,
+            }),
+            Box::new(Sphere {
+                center: V3(0.0, -100.5, -1.0),
+                radius: 100.0,
+            }),
+        ]
+    };
 
     let renderer = Renderer {
         renderer: Box::new(move |i,j| {
-            let u = i as f32 / w as f32;
-            let v = (h - 1 - j) as f32 / h as f32;
-            let ray = Ray {
-                point: origin,
-                direction: lower_left_corner + horizontal.scale(u) + vertical.scale(v),
-            };
-            let c = {
-                let t = 0.5 * (V3U::new(ray.direction).y() + 1.0);
-                V3(1.0, 1.0, 1.0).scale(1.0 - t) + V3(0.5, 0.7, 1.0).scale(t)
-            };
+            let c = (0..ns).map(|_| {
+                let u = (i as f32 + rand::random::<f32>()) / w as f32;
+                let v = ((h - 1 - j) as f32 + rand::random::<f32>()) / h as f32;
+                let ray = camera.get_ray(u,v);
+
+                scene.color(ray)
+            }).sum::<V3>().scale(1.0 / ns as f32);
 
             Color::from_v3(c)
         }),
--- src/vector.rs
+++ src/vector.rs
@@ -1,4 +1,5 @@
-use std::ops::Add;
+use std::ops::*;
+use std::iter::Sum;
 
 #[derive(Clone, Copy)]
 pub struct V3(pub f32, pub f32, pub f32);
@@ -24,6 +25,10 @@ impl V3 {
         V3(self.0 * coeff, self.1 * coeff, self.2 * coeff)
     }
 
+    pub fn normalize(self) -> V3 {
+        self.scale(1.0 / self.norm())
+    }
+
     pub fn x(&self) -> f32 {
         self.0
     }
@@ -45,12 +50,35 @@ impl Add<V3> for V3 {
     }
 }
 
+impl Sub<V3> for V3 {
+    type Output = V3;
+
+    fn sub(self, other: V3) -> V3 {
+        V3(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
+impl Sum<V3> for V3 {
+    fn sum<I>(iter: I) -> V3 where I: Iterator<Item = V3> {
+        let mut r = V3(0.0, 0.0, 0.0);
+        for i in iter {
+            r = r + i;
+        }
+
+        r
+    }
+}
+
 #[derive(Clone, Copy)]
 pub struct V3U(V3);
 
 impl V3U {
     pub fn new(v: V3) -> V3U {
-        V3U(v.scale(1.0 / v.norm()))
+        V3U(v.normalize())
+    }
+
+    pub fn as_V3(self) -> V3 {
+        self.0
     }
 
     pub fn x(&self) -> f32 {
